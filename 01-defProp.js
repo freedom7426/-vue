570f20d7400149cffefaf9b60c4be6fb8f1cdc40
freedom7426 <928240754@qq.com>
2020-10-30 13:37:17 +0800

    hjk

--- 01-defProp.js
+++ 01-defProp.js
@@ -1,1 +1,24 @@
-sdrf
+//响应式
+const obj = {}
+
+function defineReactive(obj, key, val){
+   //  对传入obj进行访问拦截
+   Object.defineProperty(obj,key, {
+      get() {
+        console.log('get '+key+ ':' +val);
+        return val;
+      },
+      set(newVal){
+         if(newVal !== val){
+            console.log('set '+ key + ':' + newVal);
+            val = newVal;
+         }
+      }
+
+   })
+   
+}
+
+defineReactive(obj, 'foo','foo')
+obj.foo
+obj.foo = "foolllllllll"
